c2033b45d88bccbe040f45e490814b29c7e6d9a5
小松果 <729631422@qq.com>
2018-05-01 18:17:34 +0800

    修改mysql类的应用4

--- thinkphp/library/think/View.php
+++ thinkphp/library/think/View.php
@@ -44,7 +44,7 @@ class View
         $baseReplace = [
             '__ROOT__'   => $root,
             '__URL__'    => $base . '/' . $request->module() . '/' . Loader::parseName($request->controller()),
-            '__STATIC__' => $root . '/static/adminlte',
+            '__STATIC__' => $root . '/static',
             '__CSS__'    => $root . '/static/css',
             '__JS__'     => $root . '/static/js',
         ];
